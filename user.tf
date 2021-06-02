0c864f118a1d63eecfb489a661f30c02551da300
Michael <ifeanyi.chemezie@gmail.com>
2021-06-02 10:01:03 -0700

    made changes to code

--- user.tf
+++ user.tf
@@ -1,14 +1,20 @@
-module "iam_iam-user" {
+module "iam_user" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-user"
-  version = "4.1.0"
-  name    = "eks-user"
+  version = "~> 3.0"
+
+  name          = "eks.user"
+  force_destroy = false
+
+  pgp_key = "keybase:test"
+
+  password_reset_required = false
 }
 
 module "iam_iam-group-with-policies" {
   source      = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
   version     = "2.3.0"
   name        = "eks-developers"
-  group_users = [("module.iam_iam-user")]
+  group_users = ["module.iam_user.name"]
   custom_group_policies = [
     { name = "EKSDevPolicy"
       policy = jsonencode({
